fb8da73cb3b7274a5b5b918d423716d3069af0a2
ritishkumar-2920 <ritish999kumar@gmail.com>
2024-12-23 20:59:20 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "gold"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "goldchain"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "GC"
 )
 
 var ID ids.ID
